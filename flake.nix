7db9c7da5a7daeb6bbb0fe240e9a24969717d309
Olle Boti <olle12boti+git@gmail.com>
2023-10-06 16:08:15 +0200

    move nix files

--- flake.nix
+++ flake.nix
@@ -2,14 +2,14 @@
   outputs = { self, nixpkgs }: {
     packages = nixpkgs.lib.genAttrs nixpkgs.lib.platforms.all (system:
       let pkgs = nixpkgs.legacyPackages.${system}; in
-      { default = pkgs.callPackage ./. { }; }
+      { default = pkgs.callPackage ./nix { }; }
     );
 
-    nixosModules.default = import ./module.nix self;
+    nixosModules.default = import ./nix/module.nix self;
 
     checks.x86_64-linux.nixos = nixpkgs.lib.nixos.runTest {
       name = "spasm";
-      imports = [ ./test.nix ];
+      imports = [ ./nix/test.nix ];
       hostPkgs = nixpkgs.legacyPackages.x86_64-linux;
       defaults.imports = [ self.nixosModules.default ];
     };
--- nix/default.nix
+++ nix/default.nix
@@ -2,7 +2,7 @@
 buildGoModule {
   pname = "spasm";
   version = "1.0.0";
-  src = ./src;
+  src = ../src;
   vendorHash = "sha256-xPkbDMVxQFiS0PPRBv+H6lDjDe7F2EO+si4lCWiVFLo=";
   meta = {
     mainProgram = "spasm";
